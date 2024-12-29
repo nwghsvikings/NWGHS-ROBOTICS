88c11074d823d2dc9636c92c7df220e7938a3871
NWGHS Robotics <roboticsnorthwest@gmail.com>
2024-12-29 13:38:56 -0500

    1358468435487548732168489
    l

--- team.js
+++ team.js
@@ -1,5 +1,97 @@
-const button = document.getElementById("r");
+const alford = document.getElementById("r");
+const keh = document.getElementById("n");
+const willy = document.getElementById("w");
+const cubehead = document.getElementById("b");
+const max = document.getElementById("m");
+const tobias = document.getElementById("t");
+const bruno = document.getElementById("bh");
+const shashider = document.getElementById("s");
+const tristante = document.getElementById("tc");
+const vindinshi = document.getElementById("v");
+const yish = document.getElementById("y");
+const rayden = document.getElementById("ra");
+const lucas = document.getElementById("l");
+const dom = document.getElementById("d");
+const cheyenne = document.getElementById("c");
+const dusty = document.getElementById("cd");
+const sarah = document.getElementById("sr");
+const log = document.getElementById("lo");
+const text = document.getElementById("changetext");
 
-button.addEventListener("click", function() {
-  alert("Button clicked!");
+alford.addEventListener("click", function() {
+  text.textContent = ("Robo Vikings #9990 Coach and Mentor");
 });
+
+keh.addEventListener("click", function() {
+  text.textContent = ("Robo Vikings #9990 Secondary Coach and Mentor");
+});
+
+willy.addEventListener("click", function() {
+  text.textContent = ("Team Co-Captain");
+});
+
+cubehead.addEventListener("click", function() {
+  text.textContent = ("Team Co-Captain");
+});
+
+max.addEventListener("click", function() {
+  text.textContent = ("Lead Programmer");
+});
+
+tobias.addEventListener("click", function() {
+  text.textContent = ("Team Leader");
+});
+
+bruno.addEventListener("click", function() {
+  text.textContent = ("Lead Builder");
+});
+
+shashider.addEventListener("click", function() {
+  text.textContent = ("Lead Docs & Lead Organization");
+});
+
+tristante.addEventListener("click", function() {
+  text.textContent = ("Builder");
+});
+
+vindinshi.addEventListener("click", function() {
+  text.textContent = ("Lead Outreach & Portfolio");
+});
+
+yish.addEventListener("click", function() {
+  text.textContent = ("Yash");
+});
+
+rayden.addEventListener("click", function() {
+  text.textContent = ("Builder");
+});
+
+lucas.addEventListener("click", function() {
+  text.textContent = ("Secondary Programmer");
+});
+
+dom.addEventListener("click", function() {
+  text.textContent = ("Builder & Organization");
+});
+
+cheyenne.addEventListener("click", function() {
+  text.textContent = ("Portfolio & Outreach");
+});
+
+dusty.addEventListener("click", function() {
+  text.textContent = ("Secondary Builder");
+});
+
+sarah.addEventListener("click", function() {
+  text.textContent = ("Portfolio & Outreach");
+});
+
+log.addEventListener("click", function() {
+  text.textContent = ("Programmer");
+});
+
+
+
+
+
+
